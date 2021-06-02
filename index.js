2cd6051dc0b788042b80170a8e160360a604d29c
davidwpacker <davidwgpacker@gmail.com>
2021-06-02 14:18:01 +1000

    typo. May need to return to this

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-const emojiContainer = document.getElementById("emoji-container")
-const emojiInput = document.getElementById("emoji-input")
-const pushBtn = document.getElementById("push-btn")
-const unshiftBtn = document.getElementById("unshift-btn")
-const popBtn = document.getElementById("pop-btn")
-const shiftBtn = document.getElementById("shift-btn")
 
 function renderEmojis() {
+    const emojiContainer = document.getElementById("emoji-container")
     emojiContainer.innerHTML = ""
     for (let i = 0; i < myEmojis.length; i++) {
         const emoji = document.createElement('span')
@@ -17,7 +12,9 @@ function renderEmojis() {
 
 renderEmojis()
 
+const pushBtn = document.getElementById("push-btn")
 pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
     if (emojiInput.value) {
         myEmojis.push(emojiInput.value)
         emojiInput.value = ""
@@ -25,20 +22,24 @@ pushBtn.addEventListener("click", function(){
     }
 })
 
+const unshiftBtn = document.getElementById("unshift-btn")
 unshiftBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
     if (emojiInput.value) {
         myEmojis.unshift(emojiInput.value)
         emojiInput.value = ""
         renderEmojis()
     }
 })
-// no value is going to get grabbed from the input so doesn't need to be checked for truthy/falsy
-popBtn.addEventListener("click", function() {
+
+const popBtn = document.getElementById("pop-btn")
+popBtn.addEventListener("click", function(){
     myEmojis.pop()
     renderEmojis()
 })
 
-shiftBtn.addEventListener("click", function() {
+const shiftBtn = document.getElementById("shift-btn")
+shiftBtn.addEventListener("click", function(){
     myEmojis.shift()
     renderEmojis()
 })
