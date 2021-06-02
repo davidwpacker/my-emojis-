06887f4d816710d41fdca2e5cfdbd441170afd58
davidwpacker <davidwgpacker@gmail.com>
2021-06-02 13:53:22 +1000

    function added

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
 
-for (let i = 0; i < myEmojis.length; i++) {
-    const emoji = document.createElement('span')
-    emoji.textContent = myEmojis[i]
-    emojiContainer.append(emoji)
+function renderEmojis() {
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
 }
+
+renderEmojis()
 
 const pushBtn = document.getElementById("push-btn")
 pushBtn.addEventListener("click", function(){
     const emojiInput = document.getElementById("emoji-input")
-    console.log(emojiInput.value)
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = "" // this empty string clears out the emoji container 
+        renderEmojis()
+    }
 })
